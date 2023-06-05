ab7fb6460813cd566dc5ea18abd5051b5815d4b8
oixankcute <minhchauxauxi1234@gmail.com>
2023-06-05 17:48:14 +0700

    model update

--- HotelManagement/src/main/java/com/swp/HotelManagement/Model/Blog.java
+++ HotelManagement/src/main/java/com/swp/HotelManagement/Model/Blog.java
@@ -2,6 +2,8 @@ package com.swp.HotelManagement.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,10 @@ public class Blog {
     private String title;
     private String content;
     private Date dateCreated;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
 
     // Constructors, getters, and setters
 }
