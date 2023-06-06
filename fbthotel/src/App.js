d8758b1da6e63a2426ea717961347e4c39b1af5a
Duong <duongcuute2002@gmail.com>
2023-06-06 22:36:41 +0700

    homepage3

--- fbthotel/src/App.js
+++ fbthotel/src/App.js
@@ -2,20 +2,29 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import React, { Component } from 'react';
+import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
+        <Main />
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">FBT Hotel</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
---
+++ fbthotel/src/components/DishdetailComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Media } from 'reactstrap';
+ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+class DishDetail{
+    render(){
+        return(
+            <div className='row'>
+                <div className='col-md-6 col-12'></div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail
